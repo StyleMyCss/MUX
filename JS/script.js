83afa6ecfcba68124c32d9f3a46a52179fa7cc27
MathiasQM <mathiasqm@gmail.com>
2022-05-09 13:29:44 +0200

    Test of javasctipt animations og svgs

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,23 @@
 document.querySelector('video').playbackRate = 0.2;
+
+// let path = document. querySelector('path')
+// let pathlength = path.getTotalLength()
+
+// path.style.strokeDasharray = pathlength + ' ' + pathlength;
+
+// path.style.strokeDashoffset = pathlength;
+
+// window.addEventListener('scroll', () => {
+
+//     //Hvor mange procent nede af side er vi?
+
+//     var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    
+//     //Hvor meget skal den tegne
+
+//     var drawLength = pathlength * scrollPercentage;
+
+//     // Tag baglæns
+
+//     path.style.strokeDashoffset = pathlength - drawLength;
+// })
